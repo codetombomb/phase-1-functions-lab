d35b3fb844b8d44dfcaeb5745a767e9859d96dd8
Jessie Guzman <jessieguzman777@gmail.com>
2022-01-20 11:03:52 -0600

    commit tree 08fc5ea120dfd1127bf9b89a69ccea6c5b28a26e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let pickUp = 43;
+    let blocks = 42;
+    return someValue = (pickUp - blocks);
+}
+let pickUp = 50
+
+function distanceFromHqInfeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet(someValue) {
+    
+}
+
+function calculatesFarePrice(start, destination) {
+    return someValue
+}
